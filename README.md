c904149eafcae906632e384700d5d94611ad0ee5
Ayush Singh <Ayush Singh>
2021-07-06 11:03:46 +0530

    Updated code and Readme.md

--- README.md
+++ README.md
@@ -1,7 +1,21 @@
-<h1>Excelmate</h1>
-
-#### Developed By Hack Club KNIT
+# Excelmate
+#### Developed By <b>Hack Club KNIT</b> with :heart:
 <br/>
 <p>
 An tool build for automating boring stuffs like working on excel sheet.
 </p>
+
+---
+
+### Terminal Output
+<br>
+
+![Screen](readme_img/terminal.png)
+
+---
+### Contribute
+<br>
+
+[![screen-gif](readme_img/discord.gif)](https://discord.gg/aywqhccgup)
+
+---
--- Script.py
+++ Script.py
@@ -1,14 +1,16 @@
+
 # import openpyxl
 import time
+from colors import bcolors
 # wb = openpyxl.load_workbook('Input.xlsx')
 # sheet = wb.get_sheet_by_name('Sheet1')
-print("------------------------Excelmate-----------------")
-for x in range(0,100,5):
-    print("Loading...."+str(x)+"%")
+print(bcolors.BOLD+bcolors.HEADER+"----------------------Excelmate-----------------"+bcolors.ENDC+bcolors.ENDC)
+for x in range(0,101,5):
+    print(bcolors.OKGREEN+"Loading...."+str(x)+"%"+bcolors.ENDC)
     time.sleep(0.25)
 
-print("---Project still under development---\n")
+print(bcolors.FAIL+"---Project still under development---\n"+bcolors.ENDC)
 time.sleep(1)
-print("Willing to contribute join us on Discord or make pull request.")
+print(bcolors.WARNING+"Willing to contribute join us on Discord or make pull request."+bcolors.ENDC)
 
 # wb.save("Output.xlsx")
